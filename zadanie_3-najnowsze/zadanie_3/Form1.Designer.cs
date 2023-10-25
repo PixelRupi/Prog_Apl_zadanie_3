b567a0b79aeaf967c832c1c0a764ae23890d2c13
PixelRupi <piotrekb.345@gmail.com>
2023-10-25 17:49:14 +0200

    Add files via upload

---
+++ zadanie_3-najnowsze/zadanie_3/Form1.Designer.cs
@@ -0,0 +1,227 @@
+﻿namespace zadanie_3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button_kwadratowa = new System.Windows.Forms.Button();
+            this.textBox_inputA = new System.Windows.Forms.TextBox();
+            this.textBox_inputB = new System.Windows.Forms.TextBox();
+            this.textBox_inputC = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button_sinX = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.textBox_tryg_Input = new System.Windows.Forms.TextBox();
+            this.button_cosX = new System.Windows.Forms.Button();
+            this.textBox_x_1 = new System.Windows.Forms.TextBox();
+            this.textBox_x_2 = new System.Windows.Forms.TextBox();
+            this.textBox_wynik_tryg = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button_kwadratowa
+            // 
+            this.button_kwadratowa.Location = new System.Drawing.Point(150, 520);
+            this.button_kwadratowa.Name = "button_kwadratowa";
+            this.button_kwadratowa.Size = new System.Drawing.Size(108, 104);
+            this.button_kwadratowa.TabIndex = 0;
+            this.button_kwadratowa.Text = "Rozwiąż funkcję kwadratowa";
+            this.button_kwadratowa.UseVisualStyleBackColor = true;
+            this.button_kwadratowa.Click += new System.EventHandler(this.button_kwadratowa_Click);
+            // 
+            // textBox_inputA
+            // 
+            this.textBox_inputA.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.textBox_inputA.Location = new System.Drawing.Point(12, 492);
+            this.textBox_inputA.Name = "textBox_inputA";
+            this.textBox_inputA.Size = new System.Drawing.Size(114, 31);
+            this.textBox_inputA.TabIndex = 1;
+            this.textBox_inputA.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox_inputB
+            // 
+            this.textBox_inputB.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.textBox_inputB.Location = new System.Drawing.Point(12, 554);
+            this.textBox_inputB.Name = "textBox_inputB";
+            this.textBox_inputB.Size = new System.Drawing.Size(114, 31);
+            this.textBox_inputB.TabIndex = 2;
+            this.textBox_inputB.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox_inputC
+            // 
+            this.textBox_inputC.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.textBox_inputC.Location = new System.Drawing.Point(11, 619);
+            this.textBox_inputC.Name = "textBox_inputC";
+            this.textBox_inputC.Size = new System.Drawing.Size(114, 31);
+            this.textBox_inputC.TabIndex = 3;
+            this.textBox_inputC.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(9, 471);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(117, 18);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Współczynnik a:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.Location = new System.Drawing.Point(9, 533);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(117, 18);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Współczynnik b:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label3.Location = new System.Drawing.Point(11, 598);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(117, 18);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Współczynnik c:";
+            // 
+            // button_sinX
+            // 
+            this.button_sinX.Location = new System.Drawing.Point(446, 543);
+            this.button_sinX.Name = "button_sinX";
+            this.button_sinX.Size = new System.Drawing.Size(75, 59);
+            this.button_sinX.TabIndex = 8;
+            this.button_sinX.Text = "sin(x)";
+            this.button_sinX.UseVisualStyleBackColor = true;
+            this.button_sinX.Click += new System.EventHandler(this.button_sinX_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(778, 16);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(10, 345);
+            this.pictureBox1.TabIndex = 9;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
+            // 
+            // textBox_tryg_Input
+            // 
+            this.textBox_tryg_Input.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.textBox_tryg_Input.Location = new System.Drawing.Point(446, 492);
+            this.textBox_tryg_Input.Name = "textBox_tryg_Input";
+            this.textBox_tryg_Input.Size = new System.Drawing.Size(177, 31);
+            this.textBox_tryg_Input.TabIndex = 10;
+            this.textBox_tryg_Input.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // button_cosX
+            // 
+            this.button_cosX.Location = new System.Drawing.Point(548, 543);
+            this.button_cosX.Name = "button_cosX";
+            this.button_cosX.Size = new System.Drawing.Size(75, 59);
+            this.button_cosX.TabIndex = 11;
+            this.button_cosX.Text = "cos(x)";
+            this.button_cosX.UseVisualStyleBackColor = true;
+            this.button_cosX.Click += new System.EventHandler(this.button_cosX_Click);
+            // 
+            // textBox_x_1
+            // 
+            this.textBox_x_1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.textBox_x_1.Location = new System.Drawing.Point(282, 520);
+            this.textBox_x_1.Name = "textBox_x_1";
+            this.textBox_x_1.Size = new System.Drawing.Size(147, 31);
+            this.textBox_x_1.TabIndex = 12;
+            this.textBox_x_1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox_x_2
+            // 
+            this.textBox_x_2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.textBox_x_2.Location = new System.Drawing.Point(282, 593);
+            this.textBox_x_2.Name = "textBox_x_2";
+            this.textBox_x_2.Size = new System.Drawing.Size(147, 31);
+            this.textBox_x_2.TabIndex = 13;
+            this.textBox_x_2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox_wynik_tryg
+            // 
+            this.textBox_wynik_tryg.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.textBox_wynik_tryg.Location = new System.Drawing.Point(641, 525);
+            this.textBox_wynik_tryg.Name = "textBox_wynik_tryg";
+            this.textBox_wynik_tryg.Size = new System.Drawing.Size(147, 31);
+            this.textBox_wynik_tryg.TabIndex = 14;
+            this.textBox_wynik_tryg.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 658);
+            this.Controls.Add(this.textBox_wynik_tryg);
+            this.Controls.Add(this.textBox_x_2);
+            this.Controls.Add(this.textBox_x_1);
+            this.Controls.Add(this.button_cosX);
+            this.Controls.Add(this.textBox_tryg_Input);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button_sinX);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox_inputC);
+            this.Controls.Add(this.textBox_inputB);
+            this.Controls.Add(this.textBox_inputA);
+            this.Controls.Add(this.button_kwadratowa);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button_kwadratowa;
+        private System.Windows.Forms.TextBox textBox_inputA;
+        private System.Windows.Forms.TextBox textBox_inputB;
+        private System.Windows.Forms.TextBox textBox_inputC;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button_sinX;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox textBox_tryg_Input;
+        private System.Windows.Forms.Button button_cosX;
+        private System.Windows.Forms.TextBox textBox_x_1;
+        private System.Windows.Forms.TextBox textBox_x_2;
+        private System.Windows.Forms.TextBox textBox_wynik_tryg;
+    }
+}
+
---
+++ zadanie_3-najnowsze/zadanie_3/Form1.cs
@@ -0,0 +1,271 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace zadanie_3
+{
+    public partial class Form1 : Form
+    {
+        double a, b, c, x_0, x_1, x_2;
+        double delta;
+
+
+        List<PointF> pkt_sinus_x = new List<PointF>();
+        List<PointF> pkt_cosus_y = new List<PointF>();  
+
+        private double Zamiana_Rad(int kat)
+        {
+            return (Math.PI * kat) / 180;
+        }
+
+        private double wart_sin_x(int kat)
+        {
+            double rad = Zamiana_Rad(kat);
+            return Math.Sin(rad);
+        }
+
+        private double wart_cos_x(int kat)
+        {
+            double rad = Zamiana_Rad(kat);
+            return Math.Cos(rad);
+        }
+
+        private void Punkty(int zasieg)
+        {
+            for (int i = 0; i< zasieg; i++)
+            {
+                float sin_y = (float)wart_sin_x(i) * (-1);
+                float cos_y = (float)wart_cos_x(i) * (-1);
+
+                float sinus_skala = pictureBox1.Height / 2;
+                float cosus_skala = pictureBox1.Height / 2;
+                float offset_prawo = 40;
+
+                pkt_sinus_x.Add(new PointF(i, sin_y * sinus_skala + sinus_skala + offset_prawo));
+                pkt_cosus_y.Add(new PointF(i, cos_y * cosus_skala + cosus_skala + offset_prawo));
+            }            
+        }
+
+        private void Refresh()
+        {
+            pictureBox1.Invalidate();
+        }
+
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            // sin(x)
+            Graphics p = this.CreateGraphics();
+            Pen pen_black = new Pen(Brushes.Black, 1.0F);
+
+            Console.WriteLine("lalalala");
+
+            if (pkt_sinus_x.Count > 0)
+            {
+                //p.DrawCurve(pen_black, pkt_sinus_x.ToArray());
+                p.DrawLine(pen_black, 0, (pictureBox1.Height / 2)+40, 1000, (pictureBox1.Height / 2)+40);
+                p.DrawLine(pen_black, 1, 1, 1, 420); // pionowa linia na poczatku
+                p.DrawLine(pen_black, 2, 40, 10, 40); // linia ograniczajaca z gory
+                p.DrawLine(pen_black, 2, 385, 10, 385); // linia ograniczajaca z gory
+                p.DrawLine(pen_black, 180, 210, 180, 195); //linia Pi/2 nr 1
+                p.DrawLine(pen_black, 360, 210, 360, 195); //linia Pi nr 2
+                p.DrawLine(pen_black, 540, 210, 540, 195); //linia Pi/2 nr 3
+                p.DrawLine(pen_black, 720, 210, 720, 195); //linia Pi nr 2
+            }
+        }
+
+        string temp;
+
+        
+
+
+        private void button_sinX_Click(object sender, EventArgs e)
+        {
+            button_cosX.Enabled = false;
+            button_sinX.Enabled = false;
+            double koord_x, koord_y;
+            Punkty(1000);
+            Refresh();
+
+
+            temp = (string)textBox_tryg_Input.Text;
+            if (double.TryParse(temp, out koord_x))
+            {
+                Console.WriteLine(koord_x);
+            }
+
+            Graphics gr = this.CreateGraphics();
+            Pen pen_red = new Pen(Brushes.Red, 1.0F);
+            Pen pen_black = new Pen(Brushes.Black, 1.0F);
+
+
+            float sin_y = (float)wart_sin_x((int)koord_x);
+            sin_y = sin_y * (pictureBox1.Height / 2);
+            Console.WriteLine(sin_y);
+            textBox_wynik_tryg.Text = sin_y.ToString();
+
+
+            gr.DrawLine(pen_red, 1, sin_y, (float)koord_x, sin_y);
+            gr.DrawCurve(pen_black, pkt_sinus_x.ToArray());
+
+
+            /*
+            float offset_X = 200, offset_Y = 80, skala = 40;
+            float sin_start = 0, sin_stop = 0;
+            
+
+            Graphics g = this.CreateGraphics();
+            Pen pen_black = new Pen(Brushes.Black, 1.0F);
+
+            
+
+            for (float x=0; x<=500; x=x+2.5F)
+            {
+                sin_start = ((float)Math.Sin(x)* skala) + offset_Y;
+                sin_stop = ((float)Math.Sin(x+2.5F)* skala) + offset_Y;
+
+                g.DrawLine(pen_black, x + offset_X, sin_start, x + 2.5F + offset_X, sin_stop);
+
+                sin_stop = sin_start;
+
+            }
+            */
+
+        }
+
+        private void button_cosX_Click(object sender, EventArgs e)
+        {
+            button_cosX.Enabled = false;
+            button_sinX.Enabled = false;
+            double koord_x, koord_y;
+            Punkty(1000);
+            Refresh();
+
+
+            temp = (string)textBox_tryg_Input.Text;
+            if (double.TryParse(temp, out koord_x))
+            {
+                Console.WriteLine(koord_x);
+            }
+
+            Graphics gr = this.CreateGraphics();
+            Pen pen_red = new Pen(Brushes.Blue, 1.0F);
+            Pen pen_black = new Pen(Brushes.Black, 1.0F);
+
+
+            float cos_y = (float)wart_cos_x((int)koord_x);
+            cos_y = cos_y * (pictureBox1.Height / 2);
+            Console.WriteLine(cos_y);
+            textBox_wynik_tryg.Text = cos_y.ToString();
+
+
+            gr.DrawLine(pen_red, 1, cos_y, (float)koord_x, cos_y);
+            gr.DrawCurve(pen_black, pkt_cosus_y.ToArray());
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+
+        //
+        //
+        // funkcja kwadratow
+        //
+        //
+
+        private void button_kwadratowa_Click(object sender, EventArgs e)
+        {
+        
+            List<PointF> kwadratowa = new List<PointF>();
+
+            Graphics p = this.CreateGraphics();
+            Pen pen_black = new Pen(Brushes.Black, 1.0F);
+            Pen pen_red = new Pen(Brushes.Red, 1.0F);
+
+            p.DrawLine(pen_black, 1, 180, 1000, 180); // poziomma linia na poczatku
+            p.DrawLine(pen_black, 1+400, 1, 1+400, 420); // pionowa linia na poczatku
+            
+
+            temp = (string)textBox_inputA.Text;
+            if (double.TryParse(temp, out a))
+            {
+                Console.WriteLine("a: " + a);
+                //Console.WriteLine(a.GetType());
+            }
+
+            temp = (string)textBox_inputB.Text;
+            if (double.TryParse(temp, out b))
+            {
+                Console.WriteLine("b: " + b);
+            }
+
+            temp = (string)textBox_inputC.Text;
+            if (double.TryParse(temp, out c))
+            {
+                Console.WriteLine("c: " + c);
+            }
+
+            delta = ((b * b) - (4 * a * c));
+            Console.WriteLine(delta);
+
+            if (delta > 0)
+            {
+                
+                delta = Math.Sqrt((double)delta);
+                x_1 = ((b * -1) - delta)+20;
+                x_2 = ((b * -1) + delta)+20;
+
+                Console.WriteLine("miejsca zerowe");
+                Console.WriteLine("x_1: " + x_1);
+                Console.WriteLine("x_2: " + x_2);
+
+                textBox_x_1.Text = "x1: " + x_1;
+                textBox_x_2.Text = "x2: " + x_2;
+
+                float koord_p = (float)(b / (2 * a) * (-1));
+                float koord_q = (float)((-1) * (delta / (4 * a)));
+
+                kwadratowa.Add(new PointF(0, (float)x_1));
+                kwadratowa.Add(new PointF(koord_p, koord_q));
+                kwadratowa.Add(new Point(0, (int)x_2));
+
+
+                p.DrawCurve(pen_red, kwadratowa.ToArray());
+
+
+            }
+            else if (delta == 0)
+            {
+                x_1 = ((b * -1)/ 2*a);
+                Console.WriteLine("x_1: " + x_1);
+                textBox_x_1.Text = "x0: " + x_1;
+            }
+            else 
+            {
+                Console.WriteLine("brak miejsc zerowych");
+                textBox_x_1.Text = "brak miejsc zerowych";
+            }
+        
+        }
+    }
+}
